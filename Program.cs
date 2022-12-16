7dab4d47a7aaad3367124f3f5150722471b9f647
eunjun123 <113625608+eunjun123@users.noreply.github.com>
2022-12-16 19:22:02 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class baek1712
+{
+    static void Main()
+    {
+        long[] inputArr = Array.ConvertAll(Console.ReadLine().Split(), long.Parse);
+        long resultNum = 0;
+        for (long i = 1; i <= 2100000001; i++)
+        {
+            if (i * inputArr[2] > inputArr[0] + inputArr[1] * i)
+            {
+                resultNum = i;
+                break;
+            }
+            else
+            {
+                resultNum = -1;
+            }
+        }
+        Console.WriteLine(resultNum);
+    }
+}
